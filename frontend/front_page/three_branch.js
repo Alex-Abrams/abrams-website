cbc26f6feb1729c28fce6bb7636db16852bbd8be
alex abrams <alexander.abrams99@gmail.com>
2022-06-30 16:12:39 -0700

    Added three_branch.js.

---
+++ frontend/front_page/three_branch.js
@@ -0,0 +1,4 @@
+// this is the master branch
+// this is practice for making sure i understand 1 working branch and master
+// next will be 2 working branches and master.
+/////////////////////////////////////////
