d9bb0d869ed21cad1aebf40fdbb3b8f90755a435
alex abrams <alexander.abrams99@gmail.com>
2022-07-06 14:26:41 -0700

    Added switching for each test action.

--- frontend/reducers/test_reducer.js
+++ frontend/reducers/test_reducer.js
@@ -8,7 +8,17 @@ const _nullUser = Object.freeze({
 });
 
 const testReducer = (state = {}, action) => {
+  Object.freeze(state);
 
+  switch(action.type) {
+    case RECEIVE_CURRENT_USER:
+      // return Object.assign({}, { id: action.currentUser.id });
+      return Object.assign({}, action.current_user); /// commit 2 for new_reducer_branch
+    case LOGOUT_CURRENT_USER:
+      return _nullUser;
+    default:
+      return state;
+  }
 };
 
 export default testReducer;
