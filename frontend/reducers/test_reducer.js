78bf4add76b23da728ea357db3b336fe4ec8bebc
alex abrams <alexander.abrams99@gmail.com>
2022-07-06 13:45:34 -0700

    Added test reducer structure.

---
+++ frontend/reducers/test_reducer.js
@@ -0,0 +1,14 @@
+import {
+  RECEIVE_CURRENT_USER,
+  LOGOUT_CURRENT_USER
+} from '../actions/session_actions';
+
+const _nullUser = Object.freeze({
+  id: null,
+});
+
+const testReducer = (state = {}, action) => {
+
+};
+
+export default testReducer;
