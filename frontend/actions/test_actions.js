f518fd61ca89e372aac66337b96af9e00a9d77f3
alex abrams <alexander.abrams99@gmail.com>
2022-07-06 19:01:26 -0700

    Commeted out the middle actions and added 2 new ones.

--- frontend/actions/test_actions.js
+++ frontend/actions/test_actions.js
@@ -4,6 +4,9 @@ export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
 export const RECEIVE_NEW_INFO = 'RECEIVE_NEW_INFO';
 export const RECEIVE_SHOPPING_CART = 'RECEIVE_SHOPPING_CART';
 
+export const RECEIVE_NEW_INFO = 'RECEIVE_NEW_INFO'; // change_branch changes
+export const RECEIVE_SHOPPING_CART = 'RECEIVE_SHOPPING_CART';
+
 export const receiveCurrentUser = current_user => ({
   type: RECEIVE_CURRENT_USER,
   current_user
@@ -13,6 +16,23 @@ export const receiveCurrentUser = current_user => ({
 //   type: LOGOUT_CURRENT_USER,
 // });
 
+ // change branch changes include the two new actions at bottom and the commented code
+ 
+// export const newAction = () => ({
+//   type: RECEIVE_NEW_INFO,
+// });
+//
+// export const receiveShoppingCart = cart => ({
+//   type: RECEIVE_SHOPPING_CART,
+// });
+//
+// export const newAction = () => ({
+//   type: RECEIVE_NEW_INFO,
+// });
+//
+// export const receiveShoppingCart = cart => ({
+//   type: RECEIVE_SHOPPING_CART,
+// });
 
 export const newAction = () => ({
   type: RECEIVE_NEW_INFO,
