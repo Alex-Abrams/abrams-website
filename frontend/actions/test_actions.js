17badb43015179d144f2c6d74433d8607678b95f
alex abrams <alexander.abrams99@gmail.com>
2022-07-06 18:56:05 -0700

    Added 2 actions for shopping cart and new info.

--- frontend/actions/test_actions.js
+++ frontend/actions/test_actions.js
@@ -1,12 +1,23 @@
 // this is the test actions for alex_branch
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_NEW_INFO = 'RECEIVE_NEW_INFO';
+export const RECEIVE_SHOPPING_CART = 'RECEIVE_SHOPPING_CART';
 
 export const receiveCurrentUser = current_user => ({
   type: RECEIVE_CURRENT_USER,
   current_user
 });
 
-export const logoutCurrentUser = () => ({
-  type: LOGOUT_CURRENT_USER,
+// export const logoutCurrentUser = () => ({
+//   type: LOGOUT_CURRENT_USER,
+// });
+
+
+export const newAction = () => ({
+  type: RECEIVE_NEW_INFO,
+});
+
+export const receiveShoppingCart = cart => ({
+  type: RECEIVE_SHOPPING_CART,
 });
