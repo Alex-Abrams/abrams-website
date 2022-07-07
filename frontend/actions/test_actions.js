7a28f4bdded3803835da0a3536974df6f36772ef
alex abrams <alexander.abrams99@gmail.com>
2022-07-06 18:24:36 -0700

    Added test_actions.js with 3 new actions.

---
+++ frontend/actions/test_actions.js
@@ -0,0 +1,12 @@
+// this is the test actions for alex_branch
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+
+export const receiveCurrentUser = current_user => ({
+  type: RECEIVE_CURRENT_USER,
+  current_user
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+});
